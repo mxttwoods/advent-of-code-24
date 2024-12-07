5ecbad33ec67b8b4cf31ab21af12209487d95400
Matthew Woods <mxttwoods@gmail.com>
2024-12-07 15:40:45 -0500

    day3 part2

--- Day3.java
+++ Day3.java
@@ -9,22 +9,25 @@ class Day3 {
     public static void main(String[] args) throws FileNotFoundException {
         String inputString = loadInputFile();
         // System.out.println(inputString);
-        Pattern pattern = Pattern.compile("mul\\((\\d+,\\d+)\\)");
+        Pattern pattern = Pattern.compile("(do\\(\\)|don't\\(\\)|mul\\((\\d+,\\d+)\\))");
         Matcher matcher = pattern.matcher(inputString);
-
-        ArrayList<Integer> ns = new ArrayList<>();
-        while (matcher.find()) {
-            String[] numbers = matcher.group(1).split(",");
-            int num1 = Integer.parseInt(numbers[0]);
-            int num2 = Integer.parseInt(numbers[1]);
-            System.out.println(num1 + " " + num2);
-            ns.add(num1 * num2);
-        }
         int sum = 0;
-        for (int i = 0; i < ns.size(); i++) {
-            sum += ns.get(i);
+        boolean mulEnabled = true;
+
+        while (matcher.find()) {
+            String match = matcher.group(1);
+            if (match.equals("do()")) {
+                mulEnabled = true;
+            } else if (match.equals("don't()")) {
+                mulEnabled = false;
+            } else if (mulEnabled) {
+                String[] numbers = matcher.group(2).split(",");
+                int num1 = Integer.parseInt(numbers[0]);
+                int num2 = Integer.parseInt(numbers[1]);
+                sum += num1 * num2;
+            }
         }
-        System.out.println(sum);
+        System.out.println("Sum of enabled multiplications: " + sum);
     }
 
     private static String loadInputFile() throws FileNotFoundException {
